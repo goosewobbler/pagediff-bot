a53bde4767299a4c3115559a58dbf6894cb8ab25
Sam Maister <sam.maister@bbc.co.uk>
2016-06-08 23:48:07 +0100

    abstract matching code

--- commands/match.js
+++ commands/match.js
@@ -1,16 +1,15 @@
 const Message = require('telegram-api/types/Message'),
       bot = require('../bot'),
-      getPage = require('../lib/get');
+      matchPage = require('../lib/match');
 
-bot.command('match <url> <regexStr>', message => {
+bot.command('match <url> <regexStr>', (message) => {
     const {url, regexStr} = message.args;
     let question1 = new Message().text(`So you want me to GET ${url} and let you know if the response body matches ${regexStr}`);
     bot.send(question1.to(message.chat.id));
-    getPage(url)
-        .then((response) => {
-            let regex = new RegExp(regexStr),
-                result = new Message().text(`The result is ${regex.test(response)}`);
+    matchPage(url, regexStr)
+        .then((result) => {
+            let resultMessage = new Message().text(`The result is ${result}`);
 
-            bot.send(result.to(message.chat.id));
+            bot.send(resultMessage.to(message.chat.id));
         })
 });
