2a6c98b3f072a5452f51b9836e2ca0794798adf0
Sam Maister <sam.maister@bbc.co.uk>
2016-06-08 23:38:56 +0100

    make fat arrow funcs more obvious

--- commands/start.js
+++ commands/start.js
@@ -2,6 +2,6 @@ const Message = require('telegram-api/types/Message'),
       bot = require('../bot'),
       start = new Message().text('YO!  I am a bot.');
 
-bot.command('start', message => {
+bot.command('start', (message) => {
     bot.send(start.to(message.chat.id));
 });
